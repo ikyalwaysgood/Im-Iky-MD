8c9cfe3dddd602157328752d55b5560e310b6997
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-29 22:04:27 +0700

    Update website.js

--- plugins/website.js
+++ plugins/website.js
@@ -3,8 +3,8 @@ let handler = async m => {
 let krtu = `web`
 m.reply(`
 ╭════┅〘 *Iky𝖔𝖋𝖋𝖎𝖈𝖎𝖆𝖑ཽ* 〙┅════╮
-> https://wa.me/6285794908894
-> https://www.instagram.com/iky_alwaysgood?igsh=MXdwbXR3a2xxNjRocA==
+> • https://wa.me/6285794908894
+> • https://www.instagram.com/iky_alwaysgood?igsh=MXdwbXR3a2xxNjRocA==
 
 `.trim()) 
 }
