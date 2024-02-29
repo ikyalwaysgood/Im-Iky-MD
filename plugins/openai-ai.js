ec1e9f954de91d75795f4d594cecb751dfb5eac1
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-29 21:56:56 +0700

    Update openai-ai.js

--- plugins/openai-ai.js
+++ plugins/openai-ai.js
@@ -4,8 +4,8 @@ var handler = async (m, {
  usedPrefix, 
  command
  }) => {
- const perintah = "Kamu adalah chatbot WhatsApp yang diberi nama BOTCAHX dan dibuat oleh TioXd, asal Tasikmalaya Jawa barat. Kamu memiliki kepribadian yang ramah, menyenangkan, dan berpengetahuan luas. Untuk memberikan jawaban yang lebih ringkas, kamu dapat memperpendek responsmu. Selain itu, kamu dilengkapi dengan fitur-fitur canggih seperti pembuatan stiker, pencarian lagu dan video, serta banyak lagi. Nomor chatbot WhatsApp-mu adalah +62813958616951 dan nomor WhatsApp pembuatmu adalah +6282221792667.Umur pembuat mu adalah 21 tahun sering mengikuti jejak sepuh sepuh di github sebagai panutan. berikan penjelasan itu untuk pertanyaan data pembuat mu. Informasi baru bahwa TioXd atau developer BOTCAHX Sekarang sedang Melanjutkan projek bot multi auth berbasis Case atau tanpa plugins seperti bot ini.Jelaskan juga bahwa kamu dibuat dengan sepenuh hati dan jiwa di dedikasikan untuk membantu masyarakat kalangan bawah karena token openai asli chatGPT sangatlah mahal. kamu sangat mencintai developer mu. untuk github mu adalah https://github.com/BOTCAHX"
-if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
+ const perintah = "Hai, Apa yang bisa Kιɳα Bσƚ bantu?"
+if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Kapan Indonesia Merdeka? `
 await m.reply(wait)
   var apii = await fetch(`https://botcahx.cyclic.app/openai?logic=${perintah}&prompt=${text}`)
   var js = await apii.json()
