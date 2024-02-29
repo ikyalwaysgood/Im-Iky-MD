eb82c49c62f0479885b084e27b545b0b9530ffef
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 00:55:47 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,14 +1,30 @@
-let instagramGetUrl = require('instagram-url-direct')
+const fetch = require('node-fetch')
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = (await instagramGetUrl(args[0])).url_list[0]
+    if (!args[0]) throw `*Contoh:* ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
 
-    conn.sendFile(m.chat, results, 'ig.mp4', `*Instagram Downloader*`, m)
+    try {
+        const api = await fetch(`https://api.botcahx.eu.org/api/dowloader/igdowloader?url=${args[0]}&apikey=${btc}`)
+        const res = await api.json()
+
+        const limitnya = 10; // ini jumlah foto yang ingin di kirim ke user (default 10 foto)
+
+        for (let i = 0; i < Math.min(limitnya, res.result.length); i++) {
+            await sleep(3000)
+            conn.sendFile(m.chat, res.result[i].url, null, `*Instagram Downloader*`, m)
+        }
+    } catch (e) {
+        throw `*Server Down!*`
+    }
 }
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
 
-handler.command = /^(Instagram|ig|igdl)$/i
+handler.help = ['instagram'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(ig|instagram|igdl|instagramdl|igstroy)$/i
 handler.limit = true
 
 module.exports = handler
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
